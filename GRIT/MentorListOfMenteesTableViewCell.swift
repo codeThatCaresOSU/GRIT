1bb97657833ea205e0464fd15e6df7215e5156da
codeThatCares <ctc@codeThatCaress-iMac.local>
2017-04-29 23:30:31 -0400

    Initial Commit. Adds mentor branch as well as the mentor table view of mentees. Currently just for looks, will implement features once database gets initiated

---
+++ GRIT/MentorListOfMenteesTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MentorListOfMenteesTableViewCell.swift
+//  GRIT
+//
+//  Created by codeThatCares on 4/29/17.
+//  Copyright © 2017 CodeThatCares. All rights reserved.
+//
+
+import UIKit
+
+class MentorListOfMenteesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var MenteeLabel: UILabel!
+    }
+
+
