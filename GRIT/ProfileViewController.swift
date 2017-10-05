15fe1ee1870c24f3ac19b3e38ba92394d400413c
loadedjd <williamsjared62@gmail.com>
2017-10-05 19:04:32 -0400

    Continued work on profile view:

--- GRIT/ProfileViewController.swift
+++ GRIT/ProfileViewController.swift
@@ -30,10 +30,12 @@ class ProfileViewController: UIViewController {
         self.doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.doneButtonPressed))
         self.navigationItem.rightBarButtonItem = self.doneButton
         
-        self.profileImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
-        self.profileImageView.backgroundColor = UIColor.red
+        self.profileImageView = UIImageView(frame: CGRect(x: 8, y: 75, width: 75, height: 75))
+        self.profileImageView.image = #imageLiteral(resourceName: "jared")
+        self.profileImageView.clipsToBounds = true
+        self.profileImageView.contentMode = .scaleAspectFill
         self.profileImageView.layer.cornerRadius = self.profileImageView.frame.width / 2
-        self.profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.profileImageView.translatesAutoresizingMaskIntoConstraints = true
         
         self.view.addSubview(self.profileImageView)
         
@@ -48,7 +50,7 @@ class ProfileViewController: UIViewController {
     }
     
     func setupConstraints() {
-        self.profileImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 8).isActive = true
-        self.profileImageView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: -8).isActive = true
+        self.profileImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: -20).isActive = true
+        self.profileImageView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 8).isActive = true
     }
 }
